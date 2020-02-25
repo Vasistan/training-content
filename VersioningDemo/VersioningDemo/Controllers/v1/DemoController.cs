1a483e367e0a0045851cd345dc34e1b14eef42e5
Vasistan Shakkaravarthi <vasistan@live.in>
2020-02-25 08:12:27 +0530

    Versioning demo sample project

---
+++ VersioningDemo/VersioningDemo/Controllers/v1/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VersioningDemo.Controllers.v1
+{
+    [ApiVersion("1.0", Deprecated = true)]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string GetV1() => "Demo Controller v1";
+    }
+}
---
+++ VersioningDemo/VersioningDemo/Controllers/v2/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VersioningDemo.Controllers.v2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string GetV2() => "Demo Controller v2";
+    }
+}
